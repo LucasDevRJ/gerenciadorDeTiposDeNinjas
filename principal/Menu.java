98966c8af9d962b5602654314b039ab30c595f02
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-26 17:40:51 -0300

    Menu do ninja básico.

--- principal/Menu.java
+++ principal/Menu.java
@@ -3,10 +3,9 @@ package principal;
 import java.util.Scanner;
 
 public class Menu {
-
+    int opcao;
     Scanner entrada = new Scanner(System.in);
     public void exibiMenuPrincipal() {
-        int opcao;
         do {
             System.out.println("----------|MENU PRINCIPAL|----------");
             System.out.println("1 - Criar um ninja básico.");
@@ -15,6 +14,25 @@ public class Menu {
             System.out.println("-------------------------------------");
             System.out.print("Digite a opção desejada: ");
             opcao = entrada.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    exibirMenuNinjaBasico();
+            }
+
         } while (opcao != 3);
+    }
+
+    public void exibirMenuNinjaBasico() {
+        do {
+            System.out.println("----------|NINJA BÁSICO|----------");
+            System.out.println("1 - Criar um ninja básico.");
+            System.out.println("2 - Mostrar informações do ninja básico.");
+            System.out.println("3 - Executar habilidade do ninja básico.");
+            System.out.println("4 - Voltar.");
+            System.out.println("-------------------------------------");
+            System.out.print("Digite a opção desejada: ");
+            opcao = entrada.nextInt();
+        } while (opcao != 4);
     }
 }
